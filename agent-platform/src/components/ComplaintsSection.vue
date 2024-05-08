b9b11a08ae9ab9c6d8de7c58701c518991b2e3d7
Angelica Rey <angelicareyq@gmail.com>
2024-05-08 06:25:06 +0200

    Add complaint section to manage the application

---
+++ agent-platform/src/components/ComplaintsSection.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+
+    <div>
+      <h1>Complaints</h1>
+      <h3>Select one to start</h3>
+    </div>
+    <div class="container">
+      <div class="column">
+        <MainCard msg="Uploaded a wrong artwork/typo"></MainCard>
+      </div>
+      <div class="column">
+        <MainCard msg="The finishing is wrong"></MainCard>
+      </div>
+      <div class="column">
+        <MainCard msg="Damaged product/order"></MainCard>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import MainCard from './MainCard.vue'
+
+export default {
+  name: 'ComplaintsSection',
+  components: {
+    MainCard
+  }
+
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+.column {
+  flex: 1;
+  margin: 0 10px;
+  padding: 20px;
+}
+</style>
---
+++ agent-platform/src/components/MainCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainCard',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+div {
+    border-radius: 20px;
+    border: 0px solid var(--Negro, #000);
+    background: #EAEFF5;
+    padding: 15px;
+}
+</style>
